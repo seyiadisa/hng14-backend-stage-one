43f027823369eb722bc3343d2e2dbd5565862a5f
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2026-04-29 21:09:48 +0100

    chore(auth): add auth env vars

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,19 @@ class Settings(BaseSettings):
 
     db_url: str = Field(alias="DATABASE_URL")
 
+    github_client_id: str = Field(alias="GITHUB_CLIENT_ID")
+    github_client_secret: str = Field(alias="GITHUB_CLIENT_SECRET")
+    github_redirect_uri: str = Field(alias="GITHUB_REDIRECT_URI")
+
+    frontend_url: str = Field(alias="FRONTEND_URL")
+    frontend_redirect_uri: str = Field(alias="FRONTEND_REDIRECT_URI")
+
+    access_token_expire_minutes: int = Field(alias="ACCESS_TOKEN_EXPIRE_MINUTES")
+    refresh_token_expire_minutes: int = Field(alias="REFRESH_TOKEN_EXPIRE_MINUTES")
+
+    jwt_secret_key: str = Field(alias="JWT_SECRET_KEY")
+    jwt_algorithm: str = Field(alias="JWT_ALGORITHM")
+
 
 @lru_cache
 def get_settings() -> Settings:
